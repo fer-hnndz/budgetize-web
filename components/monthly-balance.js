38b27532f8fadcb99c54ed6ed26f37844d47fdcd
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-08-04 17:05:26 -0600

    Changed class for className in monthly balance component

--- components/monthly-balance.js
+++ components/monthly-balance.js
@@ -5,8 +5,8 @@ export default function DefaultBalance({ income, expense }) {
                 <h1 className="font-bold text-lg dark:text-white">Income this Month</h1>
                 <span className="text-md text-lime-400">{income}</span>
 
-                <h1 class="font-bold text-lg dark:text-white">Balance</h1>
-                <span class="text-md text-blue-400">{income - expense}</span>
+                <h1 className="font-bold text-lg dark:text-white">Balance</h1>
+                <span className="text-md text-blue-400">{income - expense}</span>
                 <h1 className="font-bold text-lg dark:text-white">Expense this Month</h1>
                 <span className="text-md text-red-400">{expense}</span>
             </div>
