3b318196eb724294721dbe62a130426121cd8a5f
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-08-05 13:00:29 -0600

    added no select to tabs div

--- components/tabs.jsx
+++ components/tabs.jsx
@@ -5,7 +5,7 @@ export default function Tabs({ active }) {
     let hoverClasses = "transition ease-in duration-100 delay-75 hover:-translate-y-2 "
     let activeClass = "border-b-2 border-orange-500 border-dashed";
     return (
-        <div className="w-screen h-fit bg-white dark:bg-dark dark:text-white border-blue-500 border-b-2 border-dashed flex flex-row justify-center text-center gap-x-6 font-semibold py-3">
+        <div className="select-none w-screen h-fit bg-white dark:bg-dark dark:text-white border-blue-500 border-b-2 border-dashed flex flex-row justify-center text-center gap-x-6 font-semibold py-3">
 
             <div className={hoverClasses + (active == 0 ? activeClass : "")}>
                 <Link href="/">Accounts</Link>
