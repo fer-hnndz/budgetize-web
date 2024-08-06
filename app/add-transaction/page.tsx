f2c5f4257b1a89f752e4ba7047f71d29f5b27e57
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-08-05 18:21:40 -0600

    Added New Transactions pg and button for new account pg

---
+++ app/add-transaction/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { useState } from "react"
+import { Account } from "../../utils/account"
+
+function onChange(event: any, setter: any) {
+    setter(event.target.value)
+}
+export default function AddTransaction() {
+
+    let accounts = [
+        new Account("Checking", 1000),
+        new Account("Savings", 2000),
+    ]
+    const [selectedAccount, setSelectedAccount] = useState(accounts[0])
+
+    function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedAccount(accounts.find(account => account.name === event.target.value) as Account)
+    }
+
+    return (
+        <>
+            <div className="flex flex-col flex-auto justify-center text-center mt-4 overflow-none">
+                <h1 className="text-xl font-extrabold">New Transaction</h1>
+                <span className="text-blue-300 font-semibold">New Balance: {selectedAccount.balance}</span>
+                <select onChange={handleChange} className="shadow dark:bg-dark border border-black dark:border-white rounded w-fit self-center mt-4 p-2 font-sans font-semibold">
+                    {
+                        accounts.map((account: Account) => {
+                            return <option key={account.name} value={account.name}>{account.name}</option>
+                        })
+                    }
+                </select>
+            </div >
+        </>
+    )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import AccountsTable from '../../components/accounts-table'
 import { Account } from '../../utils/account'
 import React from 'react';
 import { Metadata } from 'next'
+import ButtonLink from "../../components/button-green";
 
 export const metadata: Metadata = {
   title: "Budgetize | Dashboard",
@@ -22,7 +23,11 @@ export default function Home() {
       <div className="flex flex-col lg:flex-row lg:gap-x-96 mx-10 content-center justify-center">
         <AccountsTable accounts={accounts} />
         <MonthlyBalance income="1000" expense="500" />
+      </div>
 
+      <div className="flex flex-auto justify-center mt-8 gap-x-4">
+        <ButtonLink href="/add-account" text="New Account" />
+        <ButtonLink href="/add-transaction" text="New Transaction" />
       </div>
     </>
   )
