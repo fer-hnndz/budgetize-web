bdc0fec73c5d712eb6860fac1e4fc8f70e53b696
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-08-06 18:15:43 -0600

    Styled locale select

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,17 +1,24 @@
-import { cookies } from "next/headers"
-import Tabs from "../../../components/tabs"
-import { NextIntlClientProvider } from "next-intl"
-import locales from "../../../utils/locales"
+"use client"
 
-export default async function Settings() {
+import Tabs from "../../../components/tabs"
+import locales from "../../../utils/locales"
+import { useState, useEffect } from "react"
+
+export default function Settings() {
+
+    let [locale, setLocale] = useState("en")
+    function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setLocale(event.target.value)
+    }
+
     return (
         <>
             <Tabs active={3} />
             <div>
-                <h1>Settings</h1>
+                <h1>Settings - {locale}</h1>
 
                 <h2>Language</h2>
-                <select>
+                <select className="p-2 bg-white dark:bg-dark dark:text-white border rounded-md border-zinc-600z" defaultValue="" onChange={handleChange}>
                     {locales.map((locale: string) => (
                         <option key={locale} value={locale}>
                             {locale}
